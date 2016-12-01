e3b8d08ce24634a5f4c7410310f7c173b808b6e8
lisahachmann <lisa.hachmann@gmail.com>
2016-12-01 09:19:58 -0500

    Actually writes to fullMem.dat now

--- memory_agc.v
+++ memory_agc.v
@@ -9,15 +9,19 @@ module Data_memory
 );
 
   reg [14:0] mem[0:2046]; // Generate array to store data from file
+
   //timing pulse is when we want to write to memory if we're addressing the erasable and are enabled by control unit
-  always @(posedge tp) begin // Update array on posedge clock
+  always @(posedge tp) begin // Update array on posedge timing pulse
     if ((regWE) && (Addr[11:10] == 2'b00)) begin // Check for write enable
-      mem[Addr] <= DataIn;
+      mem[Addr] = DataIn;
+      $writememb("fullMem.dat", mem); // Write to file
     end
     else if (regWE) begin
         $display("You are trying to write to fixed memory. Error.");
     end
   end
+
+
 
   initial begin
       $readmemb("fullMem.dat", mem); // Initially read file
