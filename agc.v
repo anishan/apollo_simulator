e1e2461d19e8874f329cf09a465405cdb7bddcbb
lisahachmann <lisa.hachmann@gmail.com>
2016-12-03 16:56:27 -0500

    Implemented more instructions

--- agc.v
+++ agc.v
@@ -16,13 +16,16 @@ wire tp4;
 wire tp5;
 wire tp6;
 wire tp7;
+wire tp8;
+wire tp9;
 wire memWE; //set with ctrl
 wire [11:0] MemAddr; //set with ctrl
 wire [14:0] DataIn; //set with ctrl
 wire [14:0]  DataOut;
 wire memtp;
+wire [29:0] temp_computation;
 
-reg extracode_flag_flag;
+reg extracode_flag
 reg [15:0] PC; //program counter
 reg [2:0] OpCode;
 reg [1:0] QC;
@@ -33,6 +36,8 @@ reg [14:0] instr;
 reg overflow_flag;
 reg [14:0] G_reg;
 reg S2; //the copy of the most sig bit for overflow checks
+reg hiddenreg; //for index
+reg index_flag; //to know the next instruction needs + the hidden reg
 
 // Encodings for Operations
 localparam tc = 3'b000;
@@ -43,14 +48,16 @@ localparam cs = 3'b100;
 localparam ts = 3'b101;
 localparam adandsu = 3'b110;
 localparam maskandmp = 3'b111;
+// the double instruction
 localparam  aAddr = 12'b000000000000;
 localparam  qAddr = 12'b000000000001;
 localparam  zAddr = 12'b000000000010;
+localparam  lAddr = 12'b000000000011;
 
 //create timing pulses
-sequence_generator sequencegen(clk, tp1, tp2, tp3, tp4, tp5, tp6, tp7);
+sequence_generator sequencegen(clk, tp1, tp2, tp3, tp4, tp5, tp6, tp7, tp8, t[9]);
 //have tp1 and tp2 together for when memory should run
-and andgate1(memtp, tp1, tp2, tp5, tp6,tp7); // and together all timing pulses where mem should run
+and andgate1(memtp, tp1, tp2, tp5, tp6,tp7, tp8, tp9); // and together all timing pulses where mem should run
 //memory
 Data_memory memory(memWE, memtp, clk, MemAddr, DataIn, DataOut);
 //decode the instruction just read from memory
@@ -63,7 +70,9 @@ InstrFetchUnit instrFetch(tp4, instr, OpCode, QC, PC, Addr12, Addr10);
 //tp 4: instruction decode
 //tp 5: 1st opportunity to save to a reg
 //tp 6: 2nd save to reg
-//tp 7: tbd
+//tp 7:
+//tp 8:
+//tp 9:
 
 always @(posedge clk) begin
     if (tp1 == 1)begin //read what PC is from Z reg
@@ -88,19 +97,24 @@ always @(posedge clk) begin
         //tp 4 is directly wired to instruction fetch unit (decoding)
     end
 
+    if (tp5 == 1) begin
+        if (index_flag) begin
+            instr <= instr + hiddenreg;
+            index_flag <=0;
+        end
+    end
 
     case (Op)
 	   tc: begin
            // transfer control
            //have it write the jump address to Z on second tp while Q is set to Z
-           if (tp5 == 1) begin
+           if (tp6 == 1) begin
                 //save Z to Q
                 memWE <=1;
                 MemAddr <= qAddr;
                 DataIn <= PC;
            end
-
-           if (tp6 == 1) begin
+           if (tp7 == 1) begin
                 //write the jump address to reg Z
                 memWE <=1;
                 MemAddr <= zAddr;
@@ -111,16 +125,61 @@ always @(posedge clk) begin
        ccsanddv: begin
             //count, compare and skip or dv
             //check extracode_flag
-            if (extracode_flag_flag) begin
+            if (extracode_flag) begin
                 //then DV
+                if (tp6) begin
+                // load G with the thing to divide by
+                    memWE <= 0;
+                    memAddr <= Addr12;
+                    G_reg <= DataOut;
+                end
+                if (tp7) begin
+                    memWE <= 0;
+                    memAddr <= aAddr;
+                    temp_computation <= {DataOut, 15'b000000000000000};
+                end
+                if (tp8) begin
+                    memWE <= 0;
+                    memAddr <= lAddr;
+                    temp_computation[14:0] <= DataOut;
+                end
+                if (tp9) begin
+                    // temp_computation<= temp_computation/G_reg;
+                    //separating quotient from remainder
+                    //have to overflow correct, quotient goes in A and remainder goes in L
+                end
+
+                //at the end, clear extracode_flag
+                extracode_flag <= 0;
+                overflow_flag <=0;
             end
             else begin
                 //then CCS
+                extracode_flag <= 0;
+                if (tp6 ) begin
+                    memWE <= 0;
+                    memAddr <= {2'b00, Addr10}; 
+
+                end
+                if (tp7) begin
+
+                end
+                if (tp8) begin
+
+                end
+                //need to set overflow
             end
        end
 
        index: begin
             //extra should be high.
+            //add the data retrieved at address specified to the next instruction -- see tp == 4 (not case)
+            if (tp5 == 1) begin
+                //save index's address to a hidden reg.
+                memAddr <= Addr12;
+                hiddenreg<= DataOut;
+                index_flag <=1;
+            end
        end
        xch: begin
             //exchange
@@ -132,11 +191,27 @@ always @(posedge clk) begin
             // if (tp6 == 1)begin
             //     memWE <=1;
             //     MemAddr <= aAddr;
+                    extracode_flag <= 0;
             // end
 
        end
        cs: begin
         //clear and subtract
+        //load reg A with @ a mem
+            if (tp6 == 1) begin
+                memWE <=0;
+                S2 <= 0;
+                extracode_flag <= 0;
+                memAddr <= Addr12;
+                G_reg <= ~DataOut;
+            end
+            if (tp7) begin
+            //save NOT version of data @mem into register A.
+                MemAddr <= aAddr;
+                memWE <= 1;
+                DataIn <= G_reg;
+            end
+
        end
        ts: begin
             // transfer to storage
@@ -144,17 +219,17 @@ always @(posedge clk) begin
             //look at overflow, save into reg A if there is some, then clear overflow
        end
        adandsu: begin
-            if (extracode_flag_flag) begin
+            if (extracode_flag) begin
            //then subtract
 
-           if (tp5 == 1) begin
+           if (tp6 == 1) begin
                //save mem[addr] given to reg G
                memWE <=0;
                MemAddr <= Addr12;
                G_reg <= DataOut;
            end
 
-           if (tp6 = 1) begin
+           if (tp7 = 1) begin
                //read what is in reg A
                memWE <= 1; //
                MemAddr <= aAddr;
@@ -162,24 +237,25 @@ always @(posedge clk) begin
                G_reg <= DataOut - G_reg; //reg A's contents -reg G's contents
            end
 
-          if (tp7 == 1) begin
+          if (tp8 == 1) begin
                //save to reg A.
                memWE <=1;
                MemAddr <= aAddr;
                DataIn <= G_reg;
+               extracode_flag <= 0;
           end
 
             end
             else begin
            //then add
-            if (tp5 == 1) begin
+            if (tp6 == 1) begin
                 //save mem[addr] given to reg G
                 memWE <=0;
                 MemAddr <= Addr12;
                 G_reg <= DataOut;
             end
 
-            if (tp6 = 1) begin
+            if (tp7 = 1) begin
                 //read what is in reg A
                 memWE <= 0;
                 MemAddr <= aAddr;
@@ -187,11 +263,12 @@ always @(posedge clk) begin
                 G_reg <= G_reg + DataOut; //reg G's contents + reg A's contents
             end
 
-           if (tp7 == 1) begin
+           if (tp8 == 1) begin
                 //save to reg A.
                 memWE <=1;
                 MemAddr <= aAddr;
                 DataIn <= G_reg;
+                extracode_flag <= 0;
            end
         end
        end
@@ -199,34 +276,60 @@ always @(posedge clk) begin
        maskandmp: begin
             if (extracode_flag) begin
             //then multiply
+            //two single precision => double precision
+                if (tp6) begin
+                    memWE <=0;
+                    memAddr <= Addr12;
+                    G_reg <= DataOut;
+                end
+                if (tp7) begin
+                    memAddr <= aAddr;
+                    memWE <=0;
+                    //overflow correction
+                    if (DataOut[14] == G_reg[14]) begin //checking the sign of both operands
+                        S2 <= 0; //sign of result is positive (pos*pos = pos, neg*neg = pos)
+                    end
+                    else begin
+                        S2 <= 1; //sign of result is negative (pos*neg = neg, and vice versa)
+                    end
+                    temp_computation <= G_reg * DataOut;
+                end
+                if (tp8) begin
+                    memWE <=1;
+                    MemAddr <= aAddr;
+                    DataIn <= {S2, temp_computation[28:16]}; //save higher bits in A with overflow correction
+                end
+                if (tp9) begin
+                    memWE <=1;
+                    MemAddr <= lAddr;
+                    DataIn <= temp_computation[15:0]; //save lower bits in L
+                    extracode_flag <= 0;
+                    overflow_flag <= 0;
+                end
             end
             else begin
             //then mask
-            if (tp5 == 1) begin
+            if (tp6 == 1) begin
                 //save mem[addr] given to reg G
                 memWE <=0;
                 MemAddr <= Addr12;
                 G_reg <= DataOut;
             end
-
-            if (tp6 = 1) begin
+            if (tp7 = 1) begin
                 //read what is in reg A
                 memWE <= 0;
                 MemAddr <= aAddr;
                 G_reg <= (G_reg & DataOut); //reg G's contents AND reg A's contents
             end
-            if (tp7 == 1) begin
+            if (tp8 == 1) begin
                  //save to reg A.
                  memWE <=1;
                  MemAddr <= aAddr;
                  DataIn <= G_reg;
+                 extracode_flag <= 0;
             end
-
-
             end
        end
     endcase
 end
-
-
 endmodule
