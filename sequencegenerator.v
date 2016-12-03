75a6f5319aaa03d09c494f6e273533ae62a33227
Anisha <Anisha>
2016-12-03 15:52:34 -0500

    adding tp9

--- sequencegenerator.v
+++ sequencegenerator.v
@@ -10,81 +10,48 @@ module sequence_generator
     output reg tp5,
     output reg tp6,
     output reg tp7,
-	output reg tp8
+	output reg tp8,
+	output reg tp9
     );
     reg [3:0] counter=0;
 
     always @(posedge clk) begin
-        counter <= (counter +1 )%7 ;
+        counter <= (counter +1 )%9 ;
         if (counter == 0)begin
+			tp9 <= 0;
             tp1 <= 1;
-            tp2 <= 0;
-            tp3 <= 0;
-            tp4 <= 0;
-            tp5 <= 0;
-            tp6 <= 0;
-            tp7 <= 0;
-			tp8 <= 0;
         end
         if (counter == 1)begin
             tp1 <= 0;
             tp2 <= 1;
-            tp3 <= 0;
-            tp4 <= 0;
-            tp5 <= 0;
-            tp6 <= 0;
-            tp7 <= 0;
-			tp8 <= 0;
         end
         if (counter == 2)begin
-            tp1 <= 0;
             tp2 <= 0;
             tp3 <= 1;
-            tp4 <= 0;
-            tp5 <= 0;
-            tp6 <= 0;
-            tp7 <= 0;
-			tp8 <= 0;
         end
         if (counter == 3)begin
-            tp1 <= 0;
-            tp2 <= 0;
             tp3 <= 0;
             tp4 <= 1;
-            tp5 <= 0;
-            tp6 <= 0;
-            tp7 <= 0;
-			tp8 <= 0;
         end
         if (counter == 4)begin
-            tp1 <= 0;
-            tp2 <= 0;
-            tp3 <= 0;
             tp4 <= 0;
             tp5 <= 1;
-            tp6 <= 0;
-            tp7 <= 0;
-			tp8 <= 0;
         end
         if (counter == 5)begin
-            tp1 <= 0;
-            tp2 <= 0;
-            tp3 <= 0;
-            tp4 <= 0;
             tp5 <= 0;
             tp6 <= 1;
-            tp7 <= 0;
-			tp8 <= 0;
         end
         if (counter == 6)begin
-            tp1 <= 0;
-            tp2 <= 0;
-            tp3 <= 0;
-            tp4 <= 0;
-            tp5 <= 0;
             tp6 <= 0;
             tp7 <= 1;
+        end
+		if (counter == 7)begin
+            tp7 <= 0;
+            tp8 <= 1;
+        end
+		if (counter == 8)begin
 			tp8 <= 0;
+			tp9 <= 1;
         end
     end
 
@@ -102,18 +69,19 @@ wire tp5;
 wire tp6;
 wire tp7;
 wire tp8;
+wire tp9;
 
 initial clk= 0;
 always #10 clk= !(clk);
 
-sequence_generator seq(clk, tp1, tp2, tp3, tp4, tp5, tp6, tp7, tp8);
+sequence_generator seq(clk, tp1, tp2, tp3, tp4, tp5, tp6, tp7, tp8, tp9);
 
 initial begin
 
 $dumpfile("seq.vcd");
 $dumpvars();
 #500
-$display("Check GTKWAVE for passing. Should see cascade of 1's from tp1 to tp8"); 
+$display("Check GTKWAVE for passing. Should see cascade of 1's from tp1 to tp9"); 
 $finish;
 
 end
