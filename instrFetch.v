66636148753f2884d2fa0b3ec49a7c3d932408e4
lisahachmann <lisa.hachmann@gmail.com>
2016-12-04 16:38:57 -0500

    Required a timing pulse

--- instrFetch.v
+++ instrFetch.v
@@ -4,7 +4,7 @@ input tp,
 input [14:0] instr,
 output reg [2:0] OpCode,
 output reg [1:0] QC,
-output reg PC,
+output reg Peripheral_C,
 output reg [11:0] Addr12,
 output reg [9:0] Addr10
 );
@@ -12,7 +12,7 @@ output reg [9:0] Addr10
 always @(posedge tp) begin
 	assign OpCode = instr[14:12];
 	assign QC = instr[11:10];
-	assign PC = instr[9];
+	assign Peripheral_C = instr[9];
 	assign Addr12 = instr[11:0];
 	assign Addr10 = instr[9:0];
 end
@@ -22,20 +22,21 @@ module quicktest();
 reg [14:0] instr;
 wire [2:0] OpCode;
 wire [1:0] QC;
-wire PC;
+wire Peripheral_C;
 wire [11:0] Addr12;
 wire [9:0] Addr10;
+reg tp4;
 
 reg dutpassed;
 
-InstrFetchUnit instrFetchie(instr, OpCode, QC, PC, Addr12, Addr10);
+InstrFetchUnit instrFetchie(tp4, instr, OpCode, QC, Peripheral_C, Addr12, Addr10);
 
 initial begin
-
+tp4 = 1;
 instr = 15'b110101100010001;
 //110 101 100 010 001
 #10
-if ((OpCode!=3'b110) || (QC!=2'b10) || (PC!=1'b1) || (Addr12!=12'b101100010001) || (Addr10!=10'b1100010001)) begin
+if ((OpCode!=3'b110) || (QC!=2'b10) || (Peripheral_C!=1'b1) || (Addr12!=12'b101100010001) || (Addr10!=10'b1100010001)) begin
 	dutpassed = 0;
 end
 else begin
