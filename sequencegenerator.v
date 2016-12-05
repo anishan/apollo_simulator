41ddd81cb868a8f03196d92cca49e93d10e5d2e5
lisahachmann <lisa.hachmann@gmail.com>
2016-12-05 11:16:20 -0500

    added TP10

--- sequencegenerator.v
+++ sequencegenerator.v
@@ -13,14 +13,15 @@ module sequence_generator
     output reg tp6,
     output reg tp7,
 	output reg tp8,
-	output reg tp9
+	output reg tp9,
+    output reg tp10
     );
     reg [3:0] counter=0;
 
     always @(posedge clk) begin
-        counter <= (counter +1 )%9 ;
+        counter <= (counter +1 )%10 ;
         if (counter == 0)begin
-			tp9 <= 0;
+			tp10 <= 0;
             tp1 <= 1;
         end
         if (counter == 1)begin
@@ -55,6 +56,10 @@ module sequence_generator
 			tp8 <= 0;
 			tp9 <= 1;
         end
+        if (counter == 9)begin
+            tp9 <= 0;
+            tp10 <= 1;
+        end
     end
 
 endmodule
@@ -72,18 +77,19 @@ wire tp6;
 wire tp7;
 wire tp8;
 wire tp9;
+wire tp10;
 
 initial clk= 0;
 always #10 clk= !(clk);
 
-sequence_generator seq(clk, tp1, tp2, tp3, tp4, tp5, tp6, tp7, tp8, tp9);
+sequence_generator seq(clk, tp1, tp2, tp3, tp4, tp5, tp6, tp7, tp8, tp9, tp10);
 
 initial begin
 
 $dumpfile("seq.vcd");
 $dumpvars();
 #500
-$display("Check GTKWAVE for passing. Should see cascade of 1's from tp1 to tp9");
+$display("Check GTKWAVE for passing. Should see cascade of 1's from tp1 to tp10");
 $finish;
 
 end
