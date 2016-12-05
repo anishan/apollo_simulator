c2a45e4e02981d7ba63263d1a06a6ca92be2e7e1
lisahachmann <lisa.hachmann@gmail.com>
2016-12-05 11:28:19 -0500

    ready to start making instructions in dat

--- invertertest.v
+++ invertertest.v
@@ -15,3 +15,20 @@ $display("instr2: %b", instr2);
 
 end
 endmodule
+
+module quicktestvalues();
+reg [14:0] instr;
+reg [14:0] value;
+reg dutpassed;
+reg [14:0] greater;
+
+initial begin
+
+assign instr = 15'b111001011000010 ;
+assign value = 15'b000000000000001;
+assign greater = instr > value;
+$display("greater: %b, instr: %b, value: %b", greater, instr, value);
+
+
+end
+endmodule
