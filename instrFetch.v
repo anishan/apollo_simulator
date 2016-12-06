0100479f84f1fd1af4ab7ca4e7da11489daf05be
lisahachmann <lisa.hachmann@gmail.com>
2016-12-06 18:46:03 -0500

    Negedge for tp

--- instrFetch.v
+++ instrFetch.v
@@ -18,34 +18,3 @@ always @(posedge tp) begin
 	assign Addr10 = instr[9:0];
 end
 endmodule
-
-module quicktest();
-reg [14:0] instr;
-wire [2:0] OpCode;
-wire [1:0] QC;
-wire Peripheral_C;
-wire [11:0] Addr12;
-wire [9:0] Addr10;
-reg tp4;
-
-reg dutpassed;
-
-InstrFetchUnit instrFetchie(tp4, instr, OpCode, QC, Peripheral_C, Addr12, Addr10);
-
-initial begin
-tp4 = 1;
-instr = 15'b110101100010001;
-//110 101 100 010 001
-#10
-if ((OpCode!=3'b110) || (QC!=2'b10) || (Peripheral_C!=1'b1) || (Addr12!=12'b101100010001) || (Addr10!=10'b1100010001)) begin
-	dutpassed = 0;
-end
-else begin
-	dutpassed = 1;
-end
-
-$display("IFETCH dut passed: %b", dutpassed);
-
-
-end
-endmodule
--- sequencegenerator.v
+++ sequencegenerator.v
@@ -18,7 +18,7 @@ module sequence_generator
     );
     reg [3:0] counter=0;
 
-    always @(posedge clk) begin
+    always @(negedge clk) begin
         counter <= (counter +1 )%10 ;
         if (counter == 0)begin
 			tp10 <= 0;
@@ -61,37 +61,5 @@ module sequence_generator
             tp10 <= 1;
         end
     end
-
-endmodule
-
-
-module quicksequencetest();
-
-reg clk;
-wire tp1;
-wire tp2;
-wire tp3;
-wire tp4;
-wire tp5;
-wire tp6;
-wire tp7;
-wire tp8;
-wire tp9;
-wire tp10;
-
-initial clk= 0;
-always #10 clk= !(clk);
-
-sequence_generator seq(clk, tp1, tp2, tp3, tp4, tp5, tp6, tp7, tp8, tp9, tp10);
-
-initial begin
-
-// $dumpfile("seq.vcd");
-// $dumpvars();
-#500
-$display("Check GTKWAVE for passing. Should see cascade of 1's from tp1 to tp10");
-$finish;
-
-end
 
 endmodule
